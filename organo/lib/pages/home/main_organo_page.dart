18aa4b5371a6ea96709c900dae61ed6ba87b5642
Sadikshya <sadikshyabhattarai200@gmail.com>
2023-10-06 20:01:33 +0545

    refresh

--- organo/lib/pages/home/main_organo_page.dart
+++ organo/lib/pages/home/main_organo_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:organo/pages/home/organo_page_body.dart';
 import 'package:organo/pages/auth/sign_up_page.dart';
 import 'package:organo/utlis/colors.dart';
 import 'package:organo/widgets/big_text.dart';
 import 'package:organo/widgets/small_text.dart';
+
+import '../../controllers/popular_product_controller.dart';
+import '../../controllers/recommended_product_controller.dart';
 
 class MainOrganoPage extends StatefulWidget {
   const MainOrganoPage({super.key});
@@ -13,50 +18,55 @@ class MainOrganoPage extends StatefulWidget {
 }
 
 class _MainOrganoPageState extends State<MainOrganoPage> {
+  Future<void> _loadResource() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            child: Container(
-              margin: EdgeInsets.only(top: 45, bottom: 15),
-              padding: EdgeInsets.only(left: 20, right: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      BigText(text: "ORGANO", color: AppColors.mainColor),
-                      Row(
-                        children: [
-                          SmallText(text: "Pokhara", color: Colors.black54),
-                          Icon(Icons.arrow_drop_down_rounded)
-                        ],
-                      )
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      width: 45,
-                      child: Icon(Icons.search, color: Colors.white),
-                      height: 45,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: AppColors.mainColor,
-                      ),
+    return RefreshIndicator(
+        child: Column(
+          children: [
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(top: 45, bottom: 15),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        BigText(text: "ORGANO", color: AppColors.mainColor),
+                        Row(
+                          children: [
+                            SmallText(text: "Pokhara", color: Colors.black54),
+                            Icon(Icons.arrow_drop_down_rounded)
+                          ],
+                        )
+                      ],
                     ),
-                  )
-                ],
+                    Center(
+                      child: Container(
+                        width: 45,
+                        child: Icon(Icons.search, color: Colors.white),
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppColors.mainColor,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-              child: SingleChildScrollView(
-            child: OrganoPageBody(),
-          )),
-        ],
-      ),
-    );
+            Expanded(
+                child: SingleChildScrollView(
+              child: OrganoPageBody(),
+            )),
+          ],
+        ),
+        onRefresh: _loadResource);
   }
 }
